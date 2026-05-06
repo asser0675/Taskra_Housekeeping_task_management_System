be6715a3b9f552f88f06fc80e961efe97658482d
asser0675 <r.porio.559223@umindanao.edu.ph>
2026-05-06 22:47:52 +0800

    Added AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,21 +4,32 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
+
 {
     /**
+
      * Register any application services.
+
      */
+
     public function register(): void
     {
         //
     }
 
     /**
+
      * Bootstrap any application services.
+
      */
     public function boot(): void
+
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
